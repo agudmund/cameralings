c2ffbadae17497f294529482fa479d3877d4011a
Ævar Guðmundsson <aevar.gudmundsson@gmail.com>
2018-11-23 08:23:39 +0000

    Unity test scene

---
+++ Unity/Assets/Ctrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+
+public class Ctrl : MonoBehaviour {
+
+    public TextMesh somethings;
+    SerialPort stream = new SerialPort("COM7", 38400);
+
+
+	// Use this for initialization
+	void Start () {
+        stream.Open();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        string value = stream.ReadLine();
+        somethings.text = value.ToString();
+    }
+}
